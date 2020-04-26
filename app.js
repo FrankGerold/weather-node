2db5e24ec09d72c8326045ed892e25e98aa2461b
Frank Gerold <FrankETSGerold@gmail.com>
2020-04-26 17:27:08 -0400

    API request for current weather in NY works
    Signed-off-by: Frank Gerold <FrankETSGerold@gmail.com>

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // WeatherStack - free tier weather API
 // Key: ab904fc7fce97ee1d90e837958baa66b
 // URI: http://api.weatherstack.com/current
+
+const request = require('postman-request')
+
+const url = 'http://api.weatherstack.com/current'
+const apiKey = 'ab904fc7fce97ee1d90e837958baa66b'
+const location = 'New York'
+const apiRequest = (url + '?access_key=' + apiKey +'&query=' + location)
+
+/*const weather = */request(apiRequest, (error, response) => {
+    let data = JSON.parse(response.body)
+    console.log(data.current)
+})
+
+// console.log(weather)
