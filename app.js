e24104b4fa96af02856d5c1992d59fbf01cec877
Frank Gerold <FrankETSGerold@gmail.com>
2020-04-28 08:36:44 -0400

    request auto-parse json and app reports current temp
    Signed-off-by: Frank Gerold <FrankETSGerold@gmail.com>

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ const request = require('postman-request')
 const url = 'http://api.weatherstack.com/current'
 const apiKey = 'ab904fc7fce97ee1d90e837958baa66b'
 const location = 'New York'
-const apiRequest = (url + '?access_key=' + apiKey +'&query=' + location)
+const units = 'f'
+const apiRequest = (url + '?access_key=' + apiKey +'&query=' + location + '&units=' + units)
 
-/*const weather = */request(apiRequest, (error, response) => {
-    let data = JSON.parse(response.body)
-    console.log(data.current)
+/*const weather = */
+request({uri: apiRequest, json: true}, (error, response) => {
+    // let data = JSON.parse(response.body)
+    let data = response.body
+
+    console.log(`In ${data.location.name}, it is ${data.current.temperature} degrees out, but it feels more like ${data.current.feelslike}!`)
+
 })
 
 // console.log(weather)
